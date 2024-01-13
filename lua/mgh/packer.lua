9e34283bcba04e53ecf0886b0d83bff13bd007ce
matt-hagemann <matt.hagemann@canonical.com>
2024-01-13 21:56:16 +0200

    basic lsp added

--- lua/mgh/packer.lua
+++ lua/mgh/packer.lua
@@ -24,5 +24,30 @@ return require('packer').startup(function(use)
   use("mbbill/undotree")
   -- Its a kind of magit
   use("tpope/vim-fugitive")
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  requires = {
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    --- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+    -- {'williamboman/mason.nvim'},
+    -- {'williamboman/mason-lspconfig.nvim'},
+
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+  }
+}
+  use {'williamboman/mason.nvim'}
+  use {'williamboman/mason-lspconfig.nvim'}
+  use {'hrsh7th/cmp-buffer'}
+  use {'hrsh7th/cmp-path'}
+  use {'saadparwaiz1/cmp_luasnip'}
+  use {'hrsh7th/cmp-nvim-lua'}
+  use {'rafamadriz/friendly-snippets'}
+
 
 end)
