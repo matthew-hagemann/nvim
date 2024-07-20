a57570145d927a5480e414dde7aa16244acde1e9
Matthew Hagemann <matt.hagemann@canonical.com>
2024-07-20 11:57:03 +0200

    remove hacky formatter

--- lua/mgh/functions.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_user_command("GoFmtReload", function()
-    vim.fn.system('gofmt -w ' .. vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)))
-    vim.cmd("edit!")
-end, {})
-
-vim.api.nvim_set_keymap("n", "<leader>cf", ":GoFmtReload<CR>", { noremap = true, silent = true })
-
