dbe53ab21c371dc778200288ac84214e11abf50d
Matthew Hagemann <matt.hagemann@canonical.com>
2024-07-21 21:00:08 +0200

    adding lualine

--- lua/mgh/packer.lua
+++ lua/mgh/packer.lua
@@ -20,6 +20,12 @@ return require("packer").startup(function(use)
 		run = ":TSUpdate",
 	})
 
+    -- lualine
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
+
 	-- formatter
 	use("mhartington/formatter.nvim")
 
@@ -55,37 +61,9 @@ return require("packer").startup(function(use)
 	-- Trouble plugin for displaying LSP errors and warnings
 	use({
 		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
+		cmd = "Trouble",
 		config = function()
-			require("trouble").setup({
-				height = 20, -- height of the trouble list when position is top or bottom
-				width = 50, -- width of the list when position is left or right
-				icons = false, -- use devicons for filenames
-				action_keys = { -- key mappings for actions in the trouble list
-					-- map to {} to remove a mapping, for example:
-					-- close = {},
-					close = "q", -- close the list
-					cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-					refresh = "r", -- manually refresh
-					jump = { "<cr>", "<tab>", "<2-leftmouse>" }, -- jump to the diagnostic or open / close folds
-					open_split = { "<c-x>" }, -- open buffer in new split
-					open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
-					open_tab = { "<c-t>" }, -- open buffer in new tab
-					jump_close = { "o" }, -- jump to the diagnostic and close the list
-					toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
-					switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
-					toggle_preview = "P", -- toggle auto_preview
-					hover = "K", -- opens a small popup with the full multiline message
-					preview = "p", -- preview the diagnostic location
-					open_code_href = "c", -- if present, open a URI with more information about the diagnostic error
-					close_folds = { "zM", "zm" }, -- close all folds
-					open_folds = { "zR", "zr" }, -- open all folds
-					toggle_fold = { "zA", "za" }, -- toggle fold of current file
-					previous = "k", -- previous item
-					next = "j", -- next item
-					help = "?", -- help menu
-				},
-			})
+			require("trouble").setup({}) -- for default options
 		end,
 	})
 
