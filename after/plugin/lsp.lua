395fac152a4d66c453afb4aa31c01fb3087fa300
Matthew Hagemann <matt.hagemann@canonical.com>
2024-11-30 10:46:02 +0200

    fix: add workaround for latest rust analyzer

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -83,3 +83,15 @@ cmp.setup({
 })
 
 vim.lsp.inlay_hint.enable(true)
+
+-- Workaround for invalid offset errors in Rust Analyzer diagnostics
+-- https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
