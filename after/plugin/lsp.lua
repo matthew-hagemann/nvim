6c232bfa55a51ba1bbc4718437855cfc3784ea5c
Matthew Hagemann <matt.hagemann@canonical.com>
2026-02-28 20:39:06 +0200

    godot support

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -37,16 +37,13 @@ end)
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-	ensure_installed = { "rust_analyzer", "gopls", "golangci_lint_ls", "clangd", "lua_ls", "ruff", "pylsp" },
+	ensure_installed = { "rust_analyzer", "gopls", "clangd", "lua_ls", "ruff", "pylsp" },
 	handlers = {
 		lsp_zero.default_setup,
 		clangd = function()
 			require("lspconfig").clangd.setup({
-				cmd = {
-					"clangd",
-					"--compile-commands-dir=_build", -- Point to compile_commands.json
-				},
-				root_dir = require("lspconfig").util.root_pattern("compile_commands.json", ".git"),
+				cmd = { "clangd", "--compile-commands-dir=_build" },
+				root_dir = require("lspconfig").util.root_pattern("_build/compile_commands.json", "compile_commands.json", ".git"),
 			})
 		end,
 		lua_ls = function()
--- lua/mgh/init.lua
+++ lua/mgh/init.lua
@@ -9,3 +9,49 @@ vim.api.nvim_create_autocmd("VimEnter", {
   command = "Copilot disable"
 })
 
+-- Server mode for godot
+
+-- paths to check for project.godot file
+local paths_to_check = {'/', '/../'}
+local is_godot_project = false
+local godot_project_path = ''
+local cwd = vim.fn.getcwd()
+
+-- iterate over paths and check
+for key, value in pairs(paths_to_check) do
+    if vim.uv.fs_stat(cwd .. value .. 'project.godot') then
+        is_godot_project = true
+        godot_project_path = cwd .. value
+        break
+    end
+end
+
+-- check if server is already running in godot project path
+local is_server_running = vim.uv.fs_stat(godot_project_path .. '/server.pipe')
+-- start server, if not already running
+if is_godot_project and not is_server_running then
+    vim.fn.serverstart(godot_project_path .. '/server.pipe')
+end
+
+-- Enable GDScript LSP (Neovim 0.11+)
+if is_godot_project then
+    vim.lsp.enable('gdscript')
+
+    -- Breakpoint commands for Godot debugging
+    vim.api.nvim_create_user_command('GodotBreakpoint', function()
+        vim.cmd('normal! obreakpoint')
+        vim.cmd('write')
+    end, {})
+    vim.keymap.set('n', '<leader>b', ':GodotBreakpoint<CR>')
+
+    vim.api.nvim_create_user_command('GodotDeleteBreakpoints', function()
+        vim.cmd('g/breakpoint/d')
+    end, {})
+    vim.keymap.set('n', '<leader>BD', ':GodotDeleteBreakpoints<CR>')
+
+    vim.api.nvim_create_user_command('GodotFindBreakpoints', function()
+        vim.cmd(':grep breakpoint | copen')
+    end, {})
+    vim.keymap.set('n', '<leader>BF', ':GodotFindBreakpoints<CR>')
+end
+
